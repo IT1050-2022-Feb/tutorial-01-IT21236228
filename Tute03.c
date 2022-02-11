5de9be8fcc8c44c02a38f9019a14d2f67f2120c0
IT21236228 <99157987+IT21236228@users.noreply.github.com>
2022-02-11 19:12:29 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -20,7 +20,7 @@ int main()
    int n1;
    int sum;
 	printf("\n\n Recursion : calculate the sum of numbers from 1 to n :\n");
-	printf("-----------------------------------------------------------\n");    
+	    
  
    printf(" Input the last number of the range starting from 1 : ");
    scanf("%d", &n1);
